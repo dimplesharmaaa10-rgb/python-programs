c4147468e8df8be8339aafaaa807901a2e3e3350
Dimple <dimplesharmaaa10@gmail.com>
2026-09-13 21:01:43 +0530

    Add odd number program

---
+++ even/odd.py
@@ -0,0 +1,6 @@
+# WAP to input a number and check whether even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
